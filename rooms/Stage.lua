0a0dd7b24237bc8538d13e894bf6cdef5f28e8e2
Dave G <34277803+SoloByte@users.noreply.github.com>
2021-07-14 16:52:48 +0200

    EnemyProjectile added.

--- rooms/Stage.lua
+++ rooms/Stage.lua
@@ -7,6 +7,7 @@ function Stage:new()
     self.area.world:addCollisionClass("Player")
     self.area.world:addCollisionClass("Enemy")
     self.area.world:addCollisionClass("Projectile", {ignores = {"Projectile", "Player"}})
+    self.area.world:addCollisionClass("EnemyProjectile", {ignores = {"EnemyProjectile", "Projectile", "Enemy"}})
     self.area.world:addCollisionClass("Collectable", {ignores = {"Projectile", "Collectable"}})
     self.main_canvas = love.graphics.newCanvas(gw, gh)
 
